eba551ebda40cc26d6193d863a10e338fc4761ee
Willian Xavier <engeletwill@gmail.com>
2020-07-16 17:42:30 -0300

    Modelo Basico professor

---
+++ Models/Professor.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace SmartSchool.WebAPI.Models
+{
+    public class Professor
+    {
+        public Professor()
+        {
+        }
+
+        public Professor(int professorId, string nome)
+        {
+            this.ProfessorId = professorId;
+            this.Nome = nome;
+
+        }
+        public int ProfessorId { get; set; }
+        public string Nome { get; set; }
+        public IEnumerable<Disciplina> Disciplinas { get; set; }
+    }
+}
