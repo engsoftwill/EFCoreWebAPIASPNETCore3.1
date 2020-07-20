3454cb3b0696b6a9a2fef2950c16034ad7f6bbd6
Willian Xavier <engeletwill@gmail.com>
2020-07-20 11:58:07 -0300

    Data Transfer Objects Professor Aluno Registrar aluno e professor

---
+++ EFCoreWebAPIASPNETCore3.1/Dtos/ProfessorDto.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace SmartSchool.WebAPI.Dtos
+{
+    public class ProfessorDto
+    {
+    public ProfessorDto()
+    {
+
+    }
+
+    public ProfessorDto(int id, int registo, string nome, string telefone, DateTime dataNasc)
+    {
+        this.Id = id;
+        this.Registo = registo;
+        this.nomecompleto = nome;
+        this.Telefone = telefone;
+        this.DataNasc = dataNasc;
+    }
+
+    public int Id { get; set; }
+    public int Registo { get; set; }
+    public string nomecompleto { get; set; }
+    public int idade { get; set; }
+    public string Telefone { get; set; }
+    public DateTime DataNasc { get; set; }
+    public DateTime DataIni { get; set; } = DateTime.Now;
+    public DateTime? DataFim { get; set; } = null;
+    public bool Ativo { get; set; } = true;
+    }
+}
