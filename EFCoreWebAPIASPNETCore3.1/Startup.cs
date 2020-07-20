9f03880f3500394312fdd0b0b593261df381f6d3
Willian Xavier <engeletwill@gmail.com>
2020-07-20 17:19:13 -0300

    configuracoes do Swagger

--- EFCoreWebAPIASPNETCore3.1/Startup.cs
+++ EFCoreWebAPIASPNETCore3.1/Startup.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,22 +35,77 @@ namespace SmartSchool.WebAPI
                 context => context.UseSqlite(Configuration.GetConnectionString("Default"))
             );
 
-        //    services.AddSingleton<IRepository, Repository>();
-            
-        //    services.AddTransient<IRepository, Repository>();
+            //    services.AddSingleton<IRepository, Repository>();
+
+            //    services.AddTransient<IRepository, Repository>();
 
             services.AddScoped<IRepository, Repository>();
 
-            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies()); //procura nas dll quem herda de profile
+            services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            })
+            .AddApiVersioning(options =>
+           {
+               options.DefaultApiVersion = new ApiVersion(1, 0);
+               options.AssumeDefaultVersionWhenUnspecified = true;
+               options.ReportApiVersions = true;
+           });
+
+            var apiProviderDescription = services.BuildServiceProvider()
+                                                .GetService<IApiVersionDescriptionProvider>();
 
             services.AddControllers()
             .AddNewtonsoftJson(
-                opt=>opt.SerializerSettings.ReferenceLoopHandling =
+                opt => opt.SerializerSettings.ReferenceLoopHandling =
                 Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies()); //procura nas dll quem herda de profile
+
+            foreach (var description in apiProviderDescription.ApiVersionDescriptions)
+            {
+
+
+
+                services.AddSwaggerGen(option =>
+                {
+                    option.SwaggerDoc(
+                        description.GroupName,
+                        new Microsoft.OpenApi.Models.OpenApiInfo()
+                        {
+                            Title = "Smart School API",
+                            Version = description.ApiVersion.ToString(),
+                            TermsOfService = new Uri("http://MeusTermosdeUso.com"),
+                            Description = "API para SmartSchool",
+                            License = new Microsoft.OpenApi.Models.OpenApiLicense
+                            {
+                                Name = "SmartSchool Licence",
+                                Url = new Uri("https://www.linkedin.com/in/engwillianxavier/")
+                            },
+                            Contact = new Microsoft.OpenApi.Models.OpenApiContact
+                            {
+                                Name = "Willian Daniel Menezes Xavier",
+                                Email = "engeletwill@gmail.com",
+                                Url = new Uri("https://www.linkedin.com/in/engwillianxavier/")
+                            }
+
+                        }
+                    );
+                    var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                    var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
+
+                    option.IncludeXmlComments(xmlCommentsFullPath);
+                });
+
+            }
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app,
+                                IWebHostEnvironment env,
+                                IApiVersionDescriptionProvider apiVersionDescriptionProvider)
         {
             if (env.IsDevelopment())
             {
@@ -57,6 +115,19 @@ namespace SmartSchool.WebAPI
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseSwagger()
+                .UseSwaggerUI(options =>
+                {
+                    foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions)
+                    {
+                        options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", 
+                            description.GroupName.ToUpperInvariant()
+                        );    
+                    }
+                    
+                    options.RoutePrefix = "";
+                });
 
             //app.UseAuthorization();
 
